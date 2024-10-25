700ac14b092e0c59bc8c0cb36f98a3d23ee0d7b7
Your Name <you@example.com>
2024-10-25 13:52:56 +0300

    s

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
+import AboutPage from './Children. Роутинг в React/AboutPage';
+import HomePage from './Children. Роутинг в React/HomePage';
 import DegreeСonversion from './Virtual DOM. Подключение библиотеки UI-компонентов/DegreeСonversion';
 import TodoList from './Virtual DOM. Подключение библиотеки UI-компонентов/List';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+
 
 function App() {
   return (
     <div className="App">
       <DegreeСonversion />
       <TodoList />
+      <Router>
+        <nav className='nav'>
+          <Link className='nav' to="/">Главная</Link> | <Link className='nav' to="/about">О нас</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Children. Роутинг в React/HomePage.js
@@ -0,0 +1,12 @@
+// HomePage.jsx
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Главная страница</h1>
+    </div>
+  );
+};
+
+export default HomePage;
